c34db8ee6eb37d0bb425f8fce871323b3d911978
fakhrilm <fpvariel@gmail.com>
2019-10-18 23:28:11 +0700

    latihan UTS no 2

--- latihan_uts.hs
+++ latihan_uts.hs
@@ -13,9 +13,19 @@ evaluate (a :* b) = evaluate a * evaluate b
 evaluate (a :/ b) = evaluate a / evaluate b
 
 {- No 1 Latihan UTS -}
+
 mapExpr :: (Float -> Float) -> Expr -> Expr
 mapExpr f (C x) = C (f x)
 mapExpr f (a :+ b) = (mapExpr f a) :+ (mapExpr f b)
 mapExpr f (a :- b) = (mapExpr f a) :- (mapExpr f b)
 mapExpr f (a :* b) = (mapExpr f a) :* (mapExpr f b)
 mapExpr f (a :/ b) = (mapExpr f a) :/ (mapExpr f b)
+
+{- No 2 Latihan UTS -}
+
+hoEval :: (Expr -> Float) -> Expr -> Float
+newEval (C x) = x
+hoEval f (a :+ b) = foldl (+) (f a) [(f b)]
+hoEval f (a :- b) = foldl (-) (f a) [(f b)]
+hoEval f (a :* b) = foldl (*) (f a) [(f b)]
+hoEval f (a :/ b) = foldl (/) (f a) [(f b)]
