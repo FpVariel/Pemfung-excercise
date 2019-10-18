ce3ba2349eebfb2c5ac7da9d07bfa78376c4b983
fakhrilm <fpvariel@gmail.com>
2019-10-18 23:56:00 +0700

    add some detail

--- latihan_uts.hs
+++ latihan_uts.hs
@@ -24,7 +24,8 @@ mapExpr f (a :/ b) = (mapExpr f a) :/ (mapExpr f b)
 {- No 2 Latihan UTS -}
 
 hoEval :: (Expr -> Float) -> Expr -> Float
-newEval (C x) = x
+-- newEval (C x) = x
+-- asume that f is 'evaluate'
 hoEval f (a :+ b) = foldl (+) (f a) [(f b)]
 hoEval f (a :- b) = foldl (-) (f a) [(f b)]
 hoEval f (a :* b) = foldl (*) (f a) [(f b)]
